ff771d3333f9f3da11a4427ea62e7b36a12aadd1
Ronak Vadhaiya <ronak.vadhaiya@somaiya.edu>
2024-04-07 19:40:39 -0400

    security controller, mongoose installation, user routes and schema

--- App.js
+++ App.js
@@ -3,16 +3,42 @@ import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
+import "dotenv/config";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+import SecurityController from "./SecurityController.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import mongoose from "mongoose";
+import session from "express-session";
 
 const app = express()
-app.use(cors());
+// app.use(cors())
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+);  
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));  
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
+SecurityController(app);
 Hello(app)
 Lab5(app)
 
---
+++ SecurityController.js
@@ -0,0 +1,15 @@
+const SecurityController = (app) => {
+    app.get("/api/session/set/:key/:value", (req, res) => {
+        const key = req.params.key;
+        const value = req.params.value;
+        req.session[key] = value;
+        res.send(`Key: ${key} and Value: ${value} are set`);
+    });
+    
+    app.get("/api/session/get/:key", (req, res) => {
+        const key = req.params.key;
+        const value = req.session[key];
+        res.send(`Value of Key: ${key} is ${value}`);
+    });
+}
+export default SecurityController
