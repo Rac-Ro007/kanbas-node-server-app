bc398f1df37ef4225a1f89d239ad2f6c88c9ab97
Ronak Vadhaiya <ronak.vadhaiya@somaiya.edu>
2024-04-19 19:14:57 -0400

    created modules dao, model and schema, updated routes

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -1,39 +1,51 @@
-import db from "../Database/index.js";
+// import db from "../Database/index.js";
+import * as dao from "./dao.js";
 
 function ModuleRoutes(app) {
-  app.get("/api/courses/:cid/modules", (req, res) => {
+  app.get("/api/courses/:cid/modules", async (req, res) => {
     const { cid } = req.params;
-    const modules = db.modules
-      .filter((m) => m.course === cid);
+    // const modules = db.modules
+    //   .filter((m) => m.course === cid);
+    const modules = await dao.findAllModules(cid);
     res.send(modules);
   });
 
-  app.post("/api/courses/:cid/modules", (req, res) => {
+  app.post("/api/courses/:cid/modules", async (req, res) => {
     const { cid } = req.params;
     const newModule = {
       ...req.body,
       course: cid,
-      _id: new Date().getTime().toString(),
+      id: new Date().getTime().toString(),
     };
-    db.modules.push(newModule);
+    await dao.createModule(newModule)
+    // db.modules.push(newModule);
     res.send(newModule);
   });
 
-  app.delete("/api/modules/:mid", (req, res) => {
+  app.delete("/api/modules/:mid", async (req, res) => {
     const { mid } = req.params;
-    db.modules = db.modules.filter((m) => m._id !== mid);
+    await dao.deleteModule(mid);
+    // db.modules = db.modules.filter((m) => m._id !== mid);
     res.sendStatus(200);
   });
 
-  app.put("/api/modules/:mid", (req, res) => {
+  app.put("/api/modules/:mid", async (req, res) => {
     const { mid } = req.params;
-    const moduleIndex = db.modules.findIndex(
-      (m) => m._id === mid);
-    db.modules[moduleIndex] = {
-      ...db.modules[moduleIndex],
-      ...req.body
-    };
+    // const moduleIndex = db.modules.findIndex(
+    //   (m) => m._id === mid);
+    // db.modules[moduleIndex] = {
+    //   ...db.modules[moduleIndex],
+    //   ...req.body
+    // };
+    const status = await dao.updateModule(mid, req.body);
     res.sendStatus(204);
+  });
+
+  app.put("/api/users/:userId", async (req, res) => {
+    const { userId } = req.params;
+    const status = await dao.updateUser(userId, req.body);
+    req.session.currentUser = await dao.findUserById(userId);
+    res.json(status);
   });
 
 }
